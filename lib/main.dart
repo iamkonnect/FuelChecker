a40afa913226a1b7918803772edb813aedd3e5fe
iamkonnect <infinity360tech@hotmail.com>
2024-12-05 16:21:49 +0300

    feat:signup page designed and welcome page modified

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My First Flutter App',
+      title: 'Fuel Checker App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WelcomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
