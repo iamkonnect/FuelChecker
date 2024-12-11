7a7b04d2981e757dec4b45aab64553cc11c703b8
iddimsangi <adammsangi@gmail.com>
2024-12-11 12:13:32 +0300

    SDK version updated and logo positioned at the center

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,19 @@
 import 'package:flutter/material.dart';
-import 'screens/welcome_screen.dart'; // Importing the WelcomeScreen
-import 'screens/login_screen.dart'; // Importing the LoginScreen
-import 'screens/fuel_type_selection_screen.dart'; // Importing the FuelTypeSelectionScreen
-import 'screens/forgot_password_screen.dart'; // Importing the ForgotPasswordScreen
-import 'screens/navigation_app.dart'; // Importing the NavigationApp
-import 'screens/nearby_screen.dart'; // Importing the NearbyScreen
-import 'screens/fuel_map_screen.dart'; // Importing the FuelMapScreen
+import 'screens/welcome_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fuel Checker',
+      title: 'My First Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const WelcomeScreen(),
-        '/login': (context) => const LoginScreen(),
-        '/fuel_type_selection': (context) => const FuelTypeSelectionScreen(),
-        '/forgot_password': (context) => const ForgotPasswordScreen(),
-        '/navigation': (context) => const NavigationApp(),
-        '/nearby': (context) => const FuelCheckerApp(), // Route for NearbyScreen
-        '/fuel_map': (context) => const FuelMapScreen(fuelType: 'Blend E5'), // Default fuel type
-      },
+      home: WelcomeScreen(),
     );
   }
 }
