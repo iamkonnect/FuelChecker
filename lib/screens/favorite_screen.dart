041f9f02a7ad234134c41e7996ddd95dfee6dff3
iamkonnect <infinity360tech@hotmail.com>
2025-01-08 19:10:21 +0300

    Updated icons in favorite screen to match fuel map screen

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -36,8 +36,8 @@ class FavoriteScreen extends StatelessWidget {
           BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black), label: 'Home'),
 BottomNavigationBarItem(icon: ImageIcon(AssetImage('lib/assets/images/Favourites.png'), color: Colors.black), label: 'Favorites'),
 BottomNavigationBarItem(icon: ImageIcon(AssetImage('lib/assets/images/Trends.png'), color: Colors.black), label: 'Trends'),
-          BottomNavigationBarItem(icon: Icon(Icons.trip_origin, color: Colors.black), label: 'My Trips'),
-          BottomNavigationBarItem(icon: Icon(Icons.near_me, color: Colors.black), label: 'Nearby'),
+BottomNavigationBarItem(icon: ImageIcon(AssetImage('lib/assets/images/my trips.png'), color: Colors.black), label: 'My Trips'),
+BottomNavigationBarItem(icon: ImageIcon(AssetImage('lib/assets/images/nearby.png'), color: Colors.black), label: 'Nearby'),
           BottomNavigationBarItem(icon: Icon(Icons.settings, color: Colors.black), label: 'Settings'),
         ],
         currentIndex: 1, // Set the current index for Favorites
