dcea846cefcfbd254a60bf04a1fbc2c8d87181dd
iddimsangi <adammsangi@gmail.com>
2025-02-15 08:39:09 +0300

    fixed signup screen layout

--- lib/screens/signup_screen_v7.dart
+++ lib/screens/signup_screen_v7.dart
@@ -21,8 +21,10 @@ class SignUpScreenV7State extends State<SignUpScreenV7> {
   final TextEditingController _fullNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
-  final TextEditingController _countryCodeController = TextEditingController(text: '+263');
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  final TextEditingController _countryCodeController =
+      TextEditingController(text: '+263');
   final TextEditingController _phoneController = TextEditingController();
 
   void _signUp() {
@@ -76,21 +78,80 @@ class SignUpScreenV7State extends State<SignUpScreenV7> {
                 const SizedBox(height: 20),
                 const Text(
                   'Welcome',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black),
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
                 const SizedBox(height: 20),
-                const Text('Register with', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                const Text('Register with',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
                 const SizedBox(height: 20),
+                // Social Media Icons
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  spacing: 10, // Reduced the spacing between icons
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(
+                          10), // Reduced padding inside the icon container
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.grey), // Add a border with a color
+                        borderRadius: BorderRadius.circular(
+                            10), // Slightly rounded corners
+                      ),
+                      child: Image.asset(
+                        'assets/images/Facebook Icon.png',
+                        width: 24, // Set a consistent width
+                        height: 24, // Set a consistent height
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Image.asset(
+                        'assets/images/apple icon logo.png',
+                        width: 24, // Set a consistent width
+                        height: 24, // Set a consistent height
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Image.asset(
+                        'assets/images/Black Google Icon.png',
+                        width: 24, // Set a consistent width
+                        height: 24, // Set a consistent height
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                // Full Name TextField
                 TextField(
                   controller: _fullNameController,
-                  decoration: InputDecoration(labelText: 'Full Name', border: OutlineInputBorder()),
+                  decoration: InputDecoration(
+                      labelText: 'Full Name', border: OutlineInputBorder()),
                 ),
                 const SizedBox(height: 20),
+                // Email TextField
                 TextField(
                   controller: _emailController,
-                  decoration: InputDecoration(labelText: 'Email', border: OutlineInputBorder()),
+                  decoration: InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
                 ),
                 const SizedBox(height: 20),
+                // Password TextField
                 TextField(
                   controller: _passwordController,
                   obscureText: _obscureText,
@@ -98,12 +159,15 @@ class SignUpScreenV7State extends State<SignUpScreenV7> {
                     labelText: 'Password',
                     border: OutlineInputBorder(),
                     suffixIcon: IconButton(
-                      icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+                      icon: Icon(_obscureText
+                          ? Icons.visibility
+                          : Icons.visibility_off),
                       onPressed: _togglePasswordVisibility,
                     ),
                   ),
                 ),
                 const SizedBox(height: 20),
+                // Confirm Password TextField
                 TextField(
                   controller: _confirmPasswordController,
                   obscureText: _obscureText,
@@ -111,12 +175,15 @@ class SignUpScreenV7State extends State<SignUpScreenV7> {
                     labelText: 'Confirm Password',
                     border: OutlineInputBorder(),
                     suffixIcon: IconButton(
-                      icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+                      icon: Icon(_obscureText
+                          ? Icons.visibility
+                          : Icons.visibility_off),
                       onPressed: _togglePasswordVisibility,
                     ),
                   ),
                 ),
                 const SizedBox(height: 20),
+                // Phone Number Input
                 Row(
                   children: [
                     Expanded(
@@ -145,23 +212,33 @@ class SignUpScreenV7State extends State<SignUpScreenV7> {
                   ],
                 ),
                 const SizedBox(height: 20),
+                // Sign Up Button
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.red,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
                     minimumSize: const Size(328, 51),
                   ),
                   onPressed: _signUp,
-                  child: const Text('Sign Up', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)),
+                  child: const Text('Sign Up',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
                 ),
                 const SizedBox(height: 20),
+                // Login TextButton
                 TextButton(
                   onPressed: () {
                     Navigator.pop(context); // Navigate back to the login screen
                   },
                   child: const Text(
                     "Already Got an account? Login",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.red),
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
                   ),
                 ),
               ],
